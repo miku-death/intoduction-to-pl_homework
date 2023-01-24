0eb807f2f7994fe29522ede4c614573c7ed0529f
miku-death <91643510+miku-death@users.noreply.github.com>
2023-01-24 19:39:44 +0900

    homework 6

---
+++ 41.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+
+namespace Homework
+{
+    class Program
+    {
+        static public void Main()
+        {
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            CountPositive(m);
+        }
+
+        // Решение массивом, сложность О(2n). Сложность линейная, как и во втором способе, но из-за прохождения массива дважды, медленнее работает.
+        static public int CountPositive(int m)
+        {
+            int result = 0;
+            int[] a = new int[m];
+
+            for(int i = 0; i < m; i++)
+                a[i] = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0; i < a.Length; i++)
+                if(a[i] > 0)
+                    result++;
+
+            return result;
+        }
+
+        static public int CountPositiveSecond(int m)
+        {
+            int result = 0;
+            for(int i = 0; i < m; i++)
+                if(Convert.ToInt32(Console.ReadLine()) > 0)
+                    result++;
+            return result;
+        }
+    }
+}
---
+++ 43.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+
+namespace Homework
+{
+    class Program
+    {
+        static public void Main()
+        {
+            int[] line1 = InputArray();
+            int[] line2 = InputArray();
+
+            // x = (b2 - b1)/(k2 - k1)
+            double x = (line2[1] - line1[1])/(line1[0] - line2[0]);
+            // подставляем х в одно из уравнений
+            double y = line1[0]*x + line1[1];
+
+            Console.WriteLine($"({x},{y})");
+        }
+
+        static public int[] InputArray()
+        {
+            int[] a = new int[2];
+
+            Console.WriteLine("k = ");
+            a[0] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("b = ");
+            a[1] = Convert.ToInt32(Console.ReadLine());
+
+            return a;
+        }
+    }
+}
